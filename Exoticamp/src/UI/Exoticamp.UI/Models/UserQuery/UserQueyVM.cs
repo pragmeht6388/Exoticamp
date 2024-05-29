1318403e4ffd6a0f7b5954e3e014365eeb72887c
Saniya-a <saniya@eng.rizvi.edu.in>
2024-05-30 00:43:32 +0530

    ui validation added

--- Exoticamp/src/UI/Exoticamp.UI/Models/UserQuery/UserQueyVM.cs
+++ Exoticamp/src/UI/Exoticamp.UI/Models/UserQuery/UserQueyVM.cs
@@ -1,10 +1,15 @@
-﻿namespace Exoticamp.UI.Models.UserQuery
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exoticamp.UI.Models.UserQuery
 {
     public class UserQueyVM
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage ="Please enter email")]
+        [EmailAddress(ErrorMessage = "Please enter valid email")]
         public string? Email { get; set; }
         public string? Query { get; set; }
+        [Required(ErrorMessage = "Please enter response before sending email")]
         public string? Response { get; set; }
         
     }
