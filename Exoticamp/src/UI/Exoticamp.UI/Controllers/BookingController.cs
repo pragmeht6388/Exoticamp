c13add544aadb5652309d19137d0b95b60a67eb5
SAUMYA TIWARI <saumya.tiwari@neosoftmail.com>
2024-06-20 16:16:39 +0530

    commit

--- Exoticamp/src/UI/Exoticamp.UI/Controllers/BookingController.cs
+++ Exoticamp/src/UI/Exoticamp.UI/Controllers/BookingController.cs
@@ -1,20 +1,22 @@
-﻿using Exoticamp.UI.Services.IRepositories;
+﻿using Exoticamp.UI.Models.Booking;
+using Exoticamp.UI.Services.IRepositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Exoticamp.UI.Controllers
 {
     public class BookingController : Controller
     {
-
+        private readonly IReviewsRepository _reviewsRepository;
         private readonly IBookingRepository _bookingRepository;
         public readonly ICampsiteDetailsRepository _campsiteDetailsRepository;
         public readonly ILocationRepository _locationRepository;
 
-        public BookingController(IBookingRepository bookingRepository, ICampsiteDetailsRepository campsiteDetailsRepository, ILocationRepository locationRepository)
+        public BookingController(IBookingRepository bookingRepository, ICampsiteDetailsRepository campsiteDetailsRepository, ILocationRepository locationRepository, IReviewsRepository reviewsRepository)
         {
             _bookingRepository = bookingRepository;
             _campsiteDetailsRepository = campsiteDetailsRepository;
             _locationRepository = locationRepository;
+            _reviewsRepository = reviewsRepository;
         }
         public IActionResult Index()
         {
@@ -31,8 +33,17 @@ namespace Exoticamp.UI.Controllers
         [HttpGet]
         public async Task<ActionResult> AdminBooking()
         {
+            var reviews = await _reviewsRepository.GetAllReviews();
             var bookings = await _bookingRepository.GetAllBookings();
-            return View(bookings);
+
+            var adminBookingVM = new AdminBookingVM
+            {
+                Bookings = bookings,
+                TotalReviewsCount = reviews.Count()
+            };
+
+            return View(adminBookingVM);
         }
+
     }
 }
---
+++ Exoticamp/src/UI/Exoticamp.UI/Models/Booking/AdminBookingVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Exoticamp.UI.Models.Booking
+{
+    public class AdminBookingVM
+    {
+        public IEnumerable<BookingVM> Bookings { get; set; }
+        public int TotalReviewsCount { get; set; }
+    }
+}
